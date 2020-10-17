649ce299cad5f69f804c8be736227853c88398c7
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2020-10-17 15:30:03 -0300

    Criação das structs

--- airplanefunc.cpp
+++ airplanefunc.cpp
@@ -1,3 +1,18 @@
 #include "airplanefunc.h"
 
+struct Info {
+	int id ;
+	int fuel ;
+};
 
+struct Airplane {
+	Info inf ;
+	Airplane *next ;
+};
+
+struct Row {
+	int qt ;
+	int cod ;
+	
+	Airplane *first ;
+};
