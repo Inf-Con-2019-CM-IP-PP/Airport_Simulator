8441479411a5bc99b94f61aca1962a0b8f643ee0
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2020-10-29 18:24:14 -0300

    Add files via upload

---
+++ airplanefunc.h
@@ -0,0 +1,30 @@
+#ifndef AIRPLANEFUNC_H
+#define AIRPLANEFUNC_H
+
+struct Info {
+	int id ;
+	int fuelStart ;
+	int fuelNow ;
+};
+
+struct Plane {
+	Info inf ;
+	Plane *next ;
+};
+
+struct Row {
+	int qt ;
+	Plane *first, *last ;
+};
+
+void createRows() ;
+
+Plane* createPlane(char c) ;
+
+void insertPlane(Plane *p, Row *r) ;
+
+void generatePlanes() ;
+
+void decreaseFuel() ;
+
+#endif
