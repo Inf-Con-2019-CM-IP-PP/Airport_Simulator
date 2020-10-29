dcf746550d327a026cfc1f6078d5b66d04417c9e
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2020-10-29 18:46:20 -0300

    Update airplanefunc.cpp

--- airplanefunc.cpp
+++ airplanefunc.cpp
@@ -3,40 +3,45 @@
 #include <stdlib.h>
 #include <time.h>
 
-Row at11, at12, at21, at22 ;
-Row dec1, dec2, dec3 ;
+Row at11, at12, at21, at22 ; // Filas de aterrissagem
+Row dec1, dec2, dec3 ; // Filas de decolagem
 
-int lastIDP = 0 ;
-int lastIDI = -1 ;
+int lastIDP = 0 ; // Ultimo ID par usado
+int lastIDI = -1 ; // Ultimo ID impar usado
 
 void createRows() {
-	Row vazia ;
-	vazia.first = NULL ; vazia.last = NULL ; vazia.qt = 0 ;
+	Row vazia ; // Cria um fila vazia
+	vazia.first = NULL ; vazia.last = NULL ; vazia.qt = 0 ; // atribui os valores iniciais
 	
+	// Atribuir a fila inicializada nas outras filas
 	at11 = vazia ; at12 = vazia ; at21 = vazia ; at22 = vazia ;
 	dec1 = vazia ; dec2 = vazia ; dec3 = vazia ;
 }
 
 Plane* createPlane(char c) {
+	// Cria um ponteiro de avião
 	Plane *p ;
 	p = (Plane *) malloc(sizeof(Plane)) ;
 	
+	// Verifica qual o tipo de avião a ser gerado
 	if (c == 'a') {
-		p->inf.id = lastIDP + 2 ;
-		p->inf.fuelStart = p->inf.fuelNow = rand()%20 + 1 ;
+		// Aterrissagem
+		p->inf.id = lastIDP + 2 ; // Atribui um avlor de inidice par novo
+		p->inf.fuelStart = p->inf.fuelNow = rand()%20 + 1 ; // Gera um combustivel aleatorio
 	} else {
-		p->inf.id = lastIDI + 2 ;
-		p->inf.fuelStart = p->inf.fuelNow = 20 ;
+		// Decolagem
+		p->inf.id = lastIDI + 2 ; // Atribui um avlor de inidice impar novo
+		p->inf.fuelStart = p->inf.fuelNow = 20 ; // Atribui o maximo de combustivel
 	}
 	
-	return p ;
+	return p ; // Retorna o ponteiro para o avião criado
 }
 
 void insertPlane(Plane *p, Row *r) {
-	if (r->first == NULL) {
+	if (r->first == NULL) { // Se a fila esta vazia realiza tal procedimento
 		p->next = NULL ;
 		r->first = r->last = p ;
-	} else {
+	} else { // Se a fila nao esta vazia, realiza o seguinte
 		p->next = r->last->next ;
 		r->last->next = p ;
 		r->last = p ;
