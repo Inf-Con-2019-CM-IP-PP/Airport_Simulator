85245a36478ddfc1d098908d86863ebfb5e9c08b
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2020-10-29 18:54:03 -0300

    apagar

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "airplanefunc.h"
 
 int main() {
@@ -12,18 +11,3 @@ int main() {
 	
 	return 0 ;
 }
-=======
-#include "airplanefunc.h"
-
-int main() {
-	createRows() ;
-	
-	for (int repet=0; repet < 10; repet++) {
-		generatePlanes() ;
-		
-		decreaseFuel() ;
-	}
-	
-	return 0 ;
-}
->>>>>>> 8441479411a5bc99b94f61aca1962a0b8f643ee0
