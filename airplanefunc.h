ca66acbb982f5767d5a24a1a93cdcf0a69880092
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2020-10-29 18:36:23 -0300

    Update airplanefunc.h

--- airplanefunc.h
+++ airplanefunc.h
@@ -1,30 +1,32 @@
 #ifndef AIRPLANEFUNC_H
 #define AIRPLANEFUNC_H
 
-struct Info {
-	int id ;
-	int fuelStart ;
-	int fuelNow ;
+struct Info { // Informacoes de cada aviao
+	int id ; // ID
+	int fuelStart ; // Combustivel inicial
+	int fuelNow ; // Combustivel atual
 };
 
-struct Plane {
-	Info inf ;
-	Plane *next ;
+struct Plane { // Aviao
+	Info inf ; // Um conjunto de informações
+	Plane *next ; // Ponteiro para o proximo avião
 };
 
-struct Row {
-	int qt ;
-	Plane *first, *last ;
+struct Row { // Fila
+	int qt ; // Quantidade de avioes na fila
+	Plane *first, *last ; // Ponteiro para o primeiro e o ultimo
 };
 
-void createRows() ;
+// Assinatura funcoes
 
-Plane* createPlane(char c) ;
+void createRows() ; // Inicializa as filas
 
-void insertPlane(Plane *p, Row *r) ;
+Plane* createPlane(char c) ; // Gera um aviao do tipo determinado e retorna um ponteiro para ele
 
-void generatePlanes() ;
+void insertPlane(Plane *p, Row *r) ; // Insere o avião na fila
 
-void decreaseFuel() ;
+void generatePlanes() ; // Gera os aviões e insere-os na menor fila
+
+void decreaseFuel() ; // Decrementa o combustivel atual dos avioes nas filas
 
 #endif
