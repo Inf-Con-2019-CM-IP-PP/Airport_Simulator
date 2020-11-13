411ac39ffa7a83bc6017626879962f63fa9c2d25
CarlosMMCosta <72697116+CarlosMMCosta@users.noreply.github.com>
2020-11-13 16:52:25 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,10 @@
+/*
+	Projeto de simulador de aeroporto - Trabalho prático da disciplina de Linguagem e Técnicas de Programação - CEFET-MG Campus Contagem
+	Ano: 2020
+	Autores: Carlos Márcio Moreira Costa, Isadellis Luisa da Paixão Teixeira, Pedro Henrique Gonçalves Pires
+	Descrição: Arquivo em que está sendo implementada a função principal, que roda as repetições de funcionamento do aeroporto,
+	 	   onde estão sendo chamadas as funções da biblioteca "airplanefunc.h".
+*/
 #include "airplanefunc.h"
 #include <stdio.h>
 
